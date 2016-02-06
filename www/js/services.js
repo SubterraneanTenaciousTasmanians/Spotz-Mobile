d83457b9feb4086091afc8d12f6c45156ce73ca4
Raphael Baskerville <raphael.baskerville@gmail.com>
2016-02-06 15:00:28 -0800

    fixed syntax error in services.js

--- www/js/services.js
+++ www/js/services.js
@@ -1,21 +1,21 @@
 angular.module('app.services', [])
 
 .factory('CameraFuncs', [function(){
-  cameraMethods = {};
+  // cameraMethods = {};
 
-  cameraMethods.takePhoto = function () {
+  // cameraMethods.takePhoto = function () {
 
-  var options = {
-    quality: 75,
-    destinationType: Camera.DestinationType.DATA_URL,
-    sourceType: Camera.PictureSourceType.CAMERA,
-    allowEdit: true,
-    encodingType: Camera.EncodingType.JPEG,
-    targetWidth: 300,
-    targetHeight: 300,
-    popoverOptions: CameraPopoverOptions,
-    saveToPhotoAlbum: true,
-  };
+  // var options = {
+  //   quality: 75,
+  //   destinationType: Camera.DestinationType.DATA_URL,
+  //   sourceType: Camera.PictureSourceType.CAMERA,
+  //   allowEdit: true,
+  //   encodingType: Camera.EncodingType.JPEG,
+  //   targetWidth: 300,
+  //   targetHeight: 300,
+  //   popoverOptions: CameraPopoverOptions,
+  //   saveToPhotoAlbum: true,
+  // };
 
   // $cordovaCamera.getPicture(options).then(function (imageData) {
   //     $scope.srcImage = 'data:image/jpeg;base64,' + imageData;
