6dc9e08095de60f3fc7c11c35f039aedc8137c79
Daniel Kim <daniel.kim1116@gmail.com>
2016-02-08 15:58:03 -0800

    server file cleaned up

--- platforms/ios/www/js/controllers.js
+++ platforms/ios/www/js/controllers.js
@@ -2,48 +2,72 @@ angular.module('app.controllers', [])
 
 .controller('map/NearMeCtrl', ['$scope', '$cordovaKeyboard', '$cordovaGeolocation', '$ionicLoading', '$ionicPlatform', function ($scope, $cordovaKeyboard, $cordovaGeolocation, $ionicLoading, $ionicPlatform) {
   //User street input
-  $scope.otherStreet = function(){
+  $scope.otherStreet = function () {
     $cordovaKeyboard.hideAccesoryBar(true);
 
     $cordovaKeyboard.disableScroll(true);
+    $cordovaKeyboard.close();
 
-    $
-  }
+    var isVisible = $cordovaKeyboard.isVisible();
+  };
+
   //Geolocation service
-  $ionicPlatform.ready(function () {
+  // $ionicPlatform.ready(function () {
+  //
+  //   $ionicLoading.show({
+  //     template: '<ion-spinner icon="bubbles"></ion-spinner><br/>Acquiring location!'
+  //   });
+  //
+  //   var positionOptions = {
+  //     enableHighAccuracy: true,
+  //     timeout: 10000,
+  //   };
+  //
+  //   $cordovaGeolocation.getCurrentPosition(positionOptions).then(function (position) {
+  //     var lat = position.coords.latitude;
+  //     var long = position.coords.longitude;
+  //
+  //     var myLatLng = new google.maps.LatLng(lat, long);
+  //
+  //     var mapOptions = {
+  //       center: myLatLng,
+  //       zoom: 15,
+  //       mapTypeId: google.maps.MapTypeId.ROADMAP,
+  //     };
+  //
+  //     var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+  //
+  //     $scope.map = map;
+  //     $ionicLoading.hide();
+  //   }, function (err) {
+  //
+  //     $ionicLoading.hide();
+  //     console.log('error in initializing the map: ', err);
+  //   });
+  // });
 
-    $ionicLoading.show({
-      template: '<ion-spinner icon="bubbles"></ion-spinner><br/>Acquiring location!'
-    });
+  google.maps.event.addDomListener(window, 'load', function() {
+          var myLatlng = new google.maps.LatLng(37.3000, -120.4833);
 
-    var positionOptions = {
-      enableHighAccuracy: true,
-      timeout: 10000,
-      maximumAge: 0,
-    };
+          var mapOptions = {
+              center: myLatlng,
+              zoom: 16,
+              mapTypeId: google.maps.MapTypeId.ROADMAP
+          };
 
-    $cordovaGeolocation.getCurrentPosition(positionOptions).then(function (position) {
-      var lat = position.coords.latitude;
-      var long = position.coords.longitude;
+          var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
-      var myLatLng = new google.maps.LatLng(lat, long);
+          navigator.geolocation.getCurrentPosition(function(pos) {
+              map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+              var myLocation = new google.maps.Marker({
+                  position: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
+                  map: map,
+                  title: "My Location"
+              });
+          });
 
-      var mapOptions = {
-        center: myLatLng,
-        zoom: 15,
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-      };
-
-      var map = new google.maps.Map(document.getElementById('map'), mapOptions);
-
-      $scope.map = map;
-      $ionicLoading.hide();
-    }, function (err) {
-      $ionicLoading.hide();
-      console.log('error in initializing the map: ', err);
-    });
-  });
-
+          $scope.map = map;
+      });
   //Launch Navigation Service
 }])
 
@@ -56,82 +80,55 @@ angular.module('app.controllers', [])
 }])
 
 .controller('signupCtrl', ['$scope', 'signupFactory', function ($scope, signupFactory) {
-  $scope.signup = function(userinfo) {
-    signupFactory.signup(userinfo).then(function(response){
+  $scope.signup = function (userinfo) {
+    signupFactory.signup(userinfo).then(function (response) {
       console.log('HERES THE RESPONSE ', response);
     });
   };
 }])
 
-.controller('parkingCtrl', function($scope) {
+.controller('parkingCtrl', function ($scope) {
 
 })
 
 .controller('pHOTOUPLOADCtrl', ['$http', '$scope', '$cordovaCamera', '$ionicPlatform', function ($http, $scope, $cordovaCamera, $ionicPlatform) {
-  // $scope.takePicture = function () {
-  //
-    // var options = {
-    //   quality: 75,
-    //   destinationType: Camera.DestinationType.DATA_URL,
-    //   sourceType: Camera.PictureSourceType.CAMERA,
-    //   allowEdit: true,
-    //   encodingType: Camera.EncodingType.JPEG,
-    //   targetWidth: 300,
-    //   targetHeight: 300,
-    //   popoverOptions: CameraPopoverOptions,
-    //   saveToPhotoAlbum: true,
-    // };
-  //
-  //   $cordovaCamera.getPicture(options).then(function (imageData) {
-  //       $scope.srcImage = 'data:image/jpeg;base64,' + imageData;
-  //     }, function (err) {
-  //       console.log(err);
-  //       // error
-  //   });
-  // }
-  //NOTE: attempt to refactor the code above
-  $scope.imgSrc;
+  $scope.takePicture = function () {
 
-  $ionicPlatform.ready(function () {
-    var options = {
-      quality: 75,
-      destinationType: Camera.DestinationType.DATA_URL,
-      sourceType: Camera.PictureSourceType.CAMERA,
-      allowEdit: true,
-      encodingType: Camera.EncodingType.JPEG,
-      targetWidth: 300,
-      targetHeight: 300,
-      popoverOptions: CameraPopoverOptions,
-      saveToPhotoAlbum: true,
-    };
+  var options = {
+    quality: 75,
+    destinationType: Camera.DestinationType.DATA_URL,
+    sourceType: Camera.PictureSourceType.CAMERA,
+    allowEdit: true,
+    encodingType: Camera.EncodingType.PNG,
+    targetWidth: 300,
+    targetHeight: 300,
+    popoverOptions: CameraPopoverOptions,
+    saveToPhotoAlbum: true,
+  };
 
-    $scope.takePicture = function() {
-      $cordovaCamera.getPicture(options).then(function (imageData) {
-        //NOTE:Use this if $scope.imgSrc doesnt update properly
-        // $scope.$apply(function () {
-        //   $scope.imgSrc = "data:image/jpeg;base64," + imageData;
-        // });
-        $scope.imgSrc = "data:image/jpeg;base64," + imageData;
+    $cordovaCamera.getPicture(options).then(function (imageData) {
+        $scope.srcImage = 'data:image/jpeg;base64,' + imageData;
       }, function (err) {
-        console.log('Error in takePicture function: ', err);
-      });
-    }
-  });
+        console.log(err);
+        // error
+    });
+  }
 
   $scope.sendPhoto = function () {
     // if ($scope.srcImage) {
-        $http.post('http://spotz-mobile.herokuapp.com/photo', $scope.srcImage).then( function(data){
-          console.log(data)
-          $scope.test = data;
-      })
+    $http.post('http://spotz-mobile.herokuapp.com/photo', $scope.srcImage).then(function (data) {
+      console.log(data);
+      $scope.test = data;
+    });
+
     // }
   };
 }])
 
-.controller('settingCtrl', function($scope) {
+.controller('settingCtrl', ['$scope', function ($scope) {
 
-})
+}])
 
-.controller('socialCtrl', function($scope) {
+.controller('socialCtrl', ['$scope', function ($scope) {
 
-})
+}])
