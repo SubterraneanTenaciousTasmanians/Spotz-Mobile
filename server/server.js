106ab109f75835cd8fc13ca6482f7f43ba9c502d
Daniel Kim <daniel.kim1116@gmail.com>
2016-02-08 16:17:50 -0800

    added in server routes and db files from desktop version

--- server/server.js
+++ server/server.js
@@ -15,6 +15,13 @@ var verifyToken = require('./routers/verifyToken');
 var port = process.env.PORT || 3000;
 
 var app = express();
+
+//CORS
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 /*
  *Global Middlewares
  */
@@ -37,6 +44,22 @@ app.use(express.static(__dirname + '/../www/'));
 // app.use('/api', verifyToken);
 //NOTE: this takes care of passport and our signin/signup
 app.use('/auth', assignTokenSignin);
+
+// using x,y Google Maps coordinates, find and return all the permit zones for that area
+app.get('/zones/:xCoord/:yCoord', function (req, res) {
+  console.log('received request for', req.params.xCoord, req.params.yCoord);
+  ParkingDB.findPermitZones([req.params.xCoord, req.params.yCoord]).then(function (data) {
+    res.status(200).send(data);
+  });
+});
+
+// Add new parking zones from the front end when a post request to /zones is made
+// this should be an an admin only feature
+app.post('/zones', function (req, res) {
+  ParkingDB.savePermitZones(req.body).then(function (data) {  //function is in parking.js)
+    res.status(201).send(data);
+  });
+});
 
 app.post('/photo', function (req, res, next) {
   res.status(200).send(req.body);
